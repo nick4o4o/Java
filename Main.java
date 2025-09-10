32587584d6e2ea426eac39441ef3347a6a253548
Nicholas Sanches Edo <nicholaspostiglioni@gmail.com>
2025-09-10 12:26:15 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Olá, mundo!");
+        System.out.println("GitHub Stats deve reconhecer esta linguagem!");
+        System.out.println("Exemplo de código em Java para aparecer no gráfico.");
+        System.out.println("Adicionar mais linhas ajuda a aumentar a porcentagem no gráfico.");
+        System.out.println("Comentários também aumentam o tamanho do arquivo.");
+        // Linha de comentário extra para aumentar o tamanho
+        // Mais uma linha de comentário
     }
 }
 
